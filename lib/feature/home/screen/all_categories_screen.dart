cfc062fbca9fd0af9a4615087c3b76384a8e4a16
ahizaz <izaz3531ahmed@gmail.com>
2026-01-02 16:49:57 +0600

    issue solved all catagories screen

--- lib/feature/home/screen/all_categories_screen.dart
+++ lib/feature/home/screen/all_categories_screen.dart
@@ -11,7 +11,7 @@ class AllCategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<HomeController>();
-    
+
     return Scaffold(
       backgroundColor: Color(0xffF5F5F5),
       appBar: AppBar(
@@ -37,38 +37,55 @@ class AllCategoriesScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            crossAxisSpacing: 16.w,
-            mainAxisSpacing: 16.h,
-            childAspectRatio: 0.95,
-          ),
-          itemCount: controller.categories.length,
-          itemBuilder: (context, index) {
-            final category = controller.categories[index];
-            return GestureDetector(
-              onTap: () {
-                Get.to(() => CategoryServicesScreen(
-                      categoryName: category['name']!,
-                    ));
-              },
-              child: _buildCategoryCard(
-                category['name']!,
-                category['icon']!,
+      body: Obx(
+        () => controller.categoryController.isLoading.value
+            ? Center(child: CircularProgressIndicator(color: Color(0xffCC0000)))
+            : controller.categoryController.categories.isEmpty
+            ? Center(
+                child: Text(
+                  "No categories available",
+                  style: GoogleFonts.roboto(
+                    fontSize: 14.sp,
+                    color: Color(0xff737373),
+                  ),
+                ),
+              )
+            : Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 16.w,
+                    mainAxisSpacing: 16.h,
+                    childAspectRatio: 0.95,
+                  ),
+                  itemCount: controller.categoryController.categories.length,
+                  itemBuilder: (context, index) {
+                    final category =
+                        controller.categoryController.categories[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Get.to(
+                          () => CategoryServicesScreen(
+                            categoryName: category.categoryName,
+                          ),
+                        );
+                      },
+                      child: _buildCategoryCard(
+                        category.categoryName,
+                        category.categoryImage,
+                      ),
+                    );
+                  },
+                ),
               ),
-            );
-          },
-        ),
       ),
     );
   }
 
-  Widget _buildCategoryCard(String name, String iconPath) {
+  Widget _buildCategoryCard(String name, String imageUrl) {
     return Container(
-       width: 116.w,
+      width: 116.w,
       decoration: BoxDecoration(
         color: Color(0xffFFFFFF),
         borderRadius: BorderRadius.circular(19.r),
@@ -83,26 +100,50 @@ class AllCategoriesScreen extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-            iconPath,
-            height: 50.h,
-            width: 50.w,
-            fit: BoxFit.contain,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.r),
+            child: Image.network(
+              imageUrl,
+              height: 50.h,
+              width: 50.w,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return Icon(
+                  Icons.category,
+                  size: 40.sp,
+                  color: Color(0xffCC0000),
+                );
+              },
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) return child;
+                return SizedBox(
+                  width: 50.w,
+                  height: 50.h,
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      strokeWidth: 2,
+                      color: Color(0xffCC0000),
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
-         SizedBox(height: 8.h),
-         Text(
-             name,
-               style: GoogleFonts.roboto(
-                            fontSize: 12.sp,
-                            color: const Color(0xff252525),
-                            fontWeight: FontWeight.w500,
-                          ),
-
-                            textAlign: TextAlign.center,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-         ),
-
+          SizedBox(height: 8.h),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 4.w),
+            child: Text(
+              name,
+              style: GoogleFonts.roboto(
+                fontSize: 12.sp,
+                color: const Color(0xff252525),
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
         ],
       ),
     );
