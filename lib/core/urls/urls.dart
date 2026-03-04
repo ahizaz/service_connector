f074a0ff92b53a27cccf438341403b63f2339351
izaz ahmed <izazahmed@joinventureai.com>
2026-03-04 10:04:52 +0600

    Save local changes before switching branch

--- lib/core/urls/urls.dart
+++ lib/core/urls/urls.dart
@@ -1,47 +1,54 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Url {
-  static const String baseUrl = "https://6zpmb4x8-8009.inc1.devtunnels.ms";
-  static const String signUp = "$baseUrl/auth/signup/";
-  static const String verifyScreen = "$baseUrl/auth/verify-otp/";
-  static const String resendOtp = "$baseUrl/auth/resend-otp/";
-  static const String forgetPassword = "$baseUrl/auth/forgot-password/";
-  static const String resetPassword = "$baseUrl/auth/reset-password/";
-  static const String updateProfile = "$baseUrl/auth/profile/";
-  static const String login = "$baseUrl/auth/login/";
-  static const String createServiceProvider = "$baseUrl/provider/providers/";
+  static String get baseUrl =>
+      dotenv.env['BASE_URL'] ?? '';
+  static String get wsBaseUrl =>
+      dotenv.env['WEBSOCKET_URL'] ?? '';
+
+  static String get signUp => '$baseUrl/auth/signup/';
+  static String get verifyScreen => '$baseUrl/auth/verify-otp/';
+  static String get resendOtp => '$baseUrl/auth/resend-otp/';
+  static String get forgetPassword => '$baseUrl/auth/forgot-password/';
+  static String get resetPassword => '$baseUrl/auth/reset-password/';
+  static String get updateProfile => '$baseUrl/auth/profile/';
+  static String get login => '$baseUrl/auth/login/';
+  static String get createServiceProvider => '$baseUrl/provider/providers/';
 
   // Dynamic submit document URL based on provider ID
   static String submitDocument(int providerId) =>
-      "$baseUrl/provider/providers/$providerId/submit-document/";
+      '$baseUrl/provider/providers/$providerId/submit-document/';
   static String uploadWorkImage(int providerId) =>
-      "$baseUrl/provider/providers/$providerId/upload-work-image/";
-  static const String getAllcatagories = "$baseUrl/provider/categories/";
-  static const String getAllproviders = "$baseUrl/provider/providers/";//
+      '$baseUrl/provider/providers/$providerId/upload-work-image/';
+  static String get getAllcatagories => '$baseUrl/provider/categories/';
+  static String get getAllproviders => '$baseUrl/provider/providers/';
 
   // Dynamic provider details URL based on provider ID
   static String getProviderDetails(int providerId) =>
-      "$baseUrl/provider/providers/$providerId/";
-  static String createConversation = "$baseUrl/message/conversations/";
-  static String getAllConversation = "$baseUrl/message/conversations/";
+      '$baseUrl/provider/providers/$providerId/';
+  static String get createConversation => '$baseUrl/message/conversations/';
+  static String get getAllConversation => '$baseUrl/message/conversations/';
   static String acceptdeclineConversation(int conversationId) =>
-      "$baseUrl/message/conversations/$conversationId/update-status/";
+      '$baseUrl/message/conversations/$conversationId/update-status/';
   static String getSpecificConversation(int conversationId) =>
-      "$baseUrl/message/messages/?conversation_id=$conversationId";
-  static String sendMessage = "$baseUrl/message/messages/";
-  static String createOffer = "$baseUrl/offer/quotations/";
+      '$baseUrl/message/messages/?conversation_id=$conversationId';
+  static String get sendMessage => '$baseUrl/message/messages/';
+  static String get createOffer => '$baseUrl/offer/quotations/';
   static String updateOfferStatus(int quotationId) =>
-      "$baseUrl/offer/quotations/$quotationId/update-status/";
+      '$baseUrl/offer/quotations/$quotationId/update-status/';
   static String allacceptedOrder(String receiverUserId) =>
-      "$baseUrl/offer/quotations/accepted-for-order/?receiver_user_id=$receiverUserId";
-  static String createOrder = "$baseUrl/offer/orders/";
-  static String getAllofferlist = "$baseUrl/offer/quotations/";
+      '$baseUrl/offer/quotations/accepted-for-order/?receiver_user_id=$receiverUserId';
+  static String get createOrder => '$baseUrl/offer/orders/';
+  static String get getAllofferlist => '$baseUrl/offer/quotations/';
   static String cancelquotebyReceiver(int quotationId) =>
-      "$baseUrl/offer/quotations/$quotationId/cancel/";
-  static String getEarnings = "$baseUrl/offer/provider-dashboard/earnings/";
-  static String getHiringList =
-      "$baseUrl/offer/provider-dashboard/hiring-list/";
-  static String getAllorder = "$baseUrl/offer/orders/";
-  static String receiverReview = "$baseUrl/offer/reviews/";
+      '$baseUrl/offer/quotations/$quotationId/cancel/';
+  static String get getEarnings =>
+      '$baseUrl/offer/provider-dashboard/earnings/';
+  static String get getHiringList =>
+      '$baseUrl/offer/provider-dashboard/hiring-list/';
+  static String get getAllorder => '$baseUrl/offer/orders/';
+  static String get receiverReview => '$baseUrl/offer/reviews/';
   static String getProviderReviews(String providerUserId) =>
-      "$baseUrl/offer/reviews/by-provider-user/?provider_user_id=$providerUserId";
-   static String providerBankdetails = "$baseUrl/provider/bank-details/";   
+      '$baseUrl/offer/reviews/by-provider-user/?provider_user_id=$providerUserId';
+  static String get providerBankdetails => '$baseUrl/provider/bank-details/';
 }
