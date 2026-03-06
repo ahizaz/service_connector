b3a0989a208deef2787979b385396ca7610c1598
izaz ahmed <izazahmed@joinventureai.com>
2026-03-06 10:39:51 +0600

    client update

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:service_connect/app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
-  runApp(const ServiceConnector());
+  runApp(const ServiceConnector());//
 }
 
 
